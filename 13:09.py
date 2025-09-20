717f7c7f86a900076b3d4cb358c9c0cbb4caf2d7
Akilamanasa <Akilamanasa2023@gmail.com>
2025-09-20 09:28:58 +0530

    Add files via upload

---
+++ 13:09.py
@@ -0,0 +1,15 @@
+###pattern4
+# n=int(input())
+# for i in range(n,0,-1):
+#    #/////print((n-i)*" ",end=" ")
+#     #////print(i*"*")
+#     for j in range(n-i):
+#         print(" ",end=" ")
+#     for j in range(i):
+#         print("*",end=" ")
+#     print("")
+
+
+
+
+
