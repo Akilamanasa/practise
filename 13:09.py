7948e9197f97f9348882684ff5c7557b5427a14d
Akilamanasa <Akilamanasa2023@gmail.com>
2025-09-20 09:25:29 +0530

    Add files via upload

--- 13:09.py
+++ 13:09.py
@@ -1,8 +1,31 @@
+#13/09/25
+####matrix
+# n=int(input("Enter the number: "))
+# for i in range(n):
+# # ///   for j in range(n):
+# # ///       print("*",end=" ")
+# # ///   print()
+#     print("* "*n)
 
-n=int(input("Enter the number: "))
- for i in range(n):
-   for j in range(n):
-     print("*",end=" ")
-   print()
-   print("* "*n)
+
+###pattern3
+# row=int(input())
+# for i in range(row,0,-1):
+#     print("*" * i)
+
+
+###pattern4
+# n=int(input())
+# for i in range(n,0,-1):
+#    #/////print((n-i)*" ",end=" ")
+#     #////print(i*"*")
+#     for j in range(n-i):
+#         print(" ",end=" ")
+#     for j in range(i):
+#         print("*",end=" ")
+#     print("")
+
+
+
+
 
