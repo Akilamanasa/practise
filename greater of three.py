1df3205818b3a968deb562dc0a6b251b0c5e4ebf
Akilamanasa <Akilamanasa2023@gmail.com>
2025-09-20 09:31:57 +0530

    Add files via upload

---
+++ greater of three.py
@@ -0,0 +1,11 @@
+#####greater of three
+# a,b,c=map(int,input("Enter the values:").split())
+# #a=int(input())
+# #b=int(input())
+# #c=int(input())
+# if (a>b):#a>=b:
+#     print("a is greater by",a)
+# elif (b>c):# b>=c:
+#     print("b is greater ",b)
+# else :
+#     print("c is greater",c)
